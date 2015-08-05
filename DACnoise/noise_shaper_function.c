2f79ac759f786a3b2a6fd03d166e78584f06db9d
Ayush Pandey <ayush.9.pandey@gmail.com>
2015-08-05 20:35:42 +0530

    Final Report Writing Inititated with a backbone structure in place. Need to fill in the contents now.

--- DACnoise/noise_shaper_function.c
+++ DACnoise/noise_shaper_function.c
@@ -1,39 +1,27 @@
 
 //The call should look like: noise_shaper(input,history,&quantErr,sos_shaper)
 
-#include<math.h>
-#include<stdio.h>
-#include "noise_shaping_filter.c"
 
-#define order 2
-
-int noise_shaper(double sample,double *history,double *quantErr, double *sos_shaper)
+int noise_shaper(double sample,double *history,double *quantErr, double *sos_shaper,int order)
 {
     //**************************************************//
     //sos_shaper is the coefficients array having 4*order + 1 elements (gain) ,
     //Gain is the first element, and the coefficients need to be defined in the main function before calling this shaper function
     //initialize and allocate memory for history in the main function and also quantErr
     //***********************************************//
-    
+                          
     double quantIn,filterOut;
-    int tdOut2=0,quantOut;
+    int quantOut=0;
     //filtering noise
-    filterOut=iir_filter(*quantErr,sos_shaper,order,history);
-    printf("%f\n",*quantErr);
+    filterOut=iir_filter(*quantErr,sos_shaper,order,history); //need to replace the order variable with the appropriate variable in the main code.
     quantIn = sample + filterOut;
-    quantOut = quant(quantIn);
+      /// - ---- Smooth out some of the double > short roundoff errors                                                         
+    if(quantIn > 0.0) quantIn += 0.5;
+    else quantIn -= 0.5;
+    quantOut = quantIn;
+
+//     quantOut = quant(quantIn);
     *quantErr= quantOut - quantIn; //noise updated
-    tdOut2 = quantOut;
-    return tdOut2;
-}
-
-//The quant function can be replaced by another quantizer model 
-double quant(double in)
-{
-
-    int out;
-
-    out=(int)in;
-    
-    return out;
+//     tdOut2 = quantOut;
+    return quantOut;
 }
